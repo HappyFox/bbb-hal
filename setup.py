9cdf2227de361f9d55485b53fa52c3c6b1008f18
HappyFox <keith.baston@gmail.com>
2017-11-14 12:39:53 -0800

    fixing typo in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
-o
+
 setup(name='bbb_hal',
       version='1',
-      package_dir = {'': 'src'}
+      package_dir = {'': 'src'},
       packages=['bbb_hal'],
       )
 
