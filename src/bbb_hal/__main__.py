551603b2127eb5467b365d09539d5083485a63c8
HappyFox <keith.baston@gmail.com>
2017-11-16 09:14:07 -0800

    Switching over to a proper python daemon, using the python_daemon lib

--- src/bbb_hal/__main__.py
+++ src/bbb_hal/__main__.py
@@ -1,15 +1,38 @@
-import os
-import sys
+#standard python libs
+import logging
+import time
 
+#third party libs
+from daemon import runner
 
-if __name__ == "__main__":
-    if os.getuid() != 0:
-        print("Must run as root!")
-        sys.exit(1)
+class App():
 
+    def __init__(self):
+        self.stdin_path = '/dev/null'
+        self.stdout_path = '/dev/tty'
+        self.stderr_path = '/dev/tty'
+        self.pidfile_path =  '/var/run/bbb-hal/bbb-hal.pid'
+        self.pidfile_timeout = 5
 
-    print("YO")
-    with open("/home/baston/boo", "w") as boo:
-        boo.write("hello")
+    def run(self):
+        while True:
+            #Main code goes here ...
+            #Note that logger level needs to be set to logging.DEBUG before this shows up in the logs
+            logger.debug("Debug message")
+            logger.info("Info message")
+            logger.warn("Warning message")
+            logger.error("Error message")
+            time.sleep(10)
 
+app = App()
+logger = logging.getLogger("DaemonLog")
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler = logging.FileHandler("/var/log/bbb-hal/bbb-hal.log")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
+daemon_runner = runner.DaemonRunner(app)
+#This ensures that the logger file handle does not get closed during daemonization
+daemon_runner.daemon_context.files_preserve=[handler.stream]
+daemon_runner.do_action()
